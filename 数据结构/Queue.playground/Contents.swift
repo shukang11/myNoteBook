0fcab350db2b6e4fffecb4706b704d1b457f02ce
tree <sunshukang30@163.com>
2019-03-18 17:36:54 +0800

    提高了出栈时候效率

--- 数据结构/Queue.playground/Contents.swift
+++ 数据结构/Queue.playground/Contents.swift
@@ -3,18 +3,22 @@ import UIKit
 // 队列可以保证元素存入和取出的顺序是先进先出(first-in first-out, FIFO)
 
 struct Queue<T> {
-    var queue: [T] = []
+    var queue: [T?] = []
+    var head: Int = 0 // 通过index 可以提高出栈时候的效率
     
     public var isEmpty: Bool {
-        return queue.isEmpty
+        return count == 0
     }
     
     public var count: Int {
-        return queue.count
+        return queue.count - head
     }
     
     public var front: T? {
-        return queue.first
+        if isEmpty {
+            return nil
+        }
+        return queue[head]
     }
     
     public mutating func enqueue(_ element: T) {
@@ -22,14 +26,19 @@ struct Queue<T> {
     }
     
     public mutating func dequeue() -> T? {
-        if isEmpty {
-            return nil
+        guard head < queue.count, let element = queue[head] else { return nil }
+        queue[head] = nil
+        head += 1
+        let percentage = Double(head) / Double(queue.count)
+        if queue.count > 50 && percentage > 0.25 {
+            queue.removeFirst(head)
+            head = 0
         }
-        return queue.removeFirst()
+        return element
     }
 }
 
-var queue = Queue.init(queue: ["1A"])
+var queue = Queue.init(queue: ["1A"], head: 0)
 queue.enqueue("2B")
 queue.enqueue("3C")
 queue.dequeue()
