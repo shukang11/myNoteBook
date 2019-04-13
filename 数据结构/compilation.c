26eff36168078412d9116214f3ac3c1ecd9c6d97
jiazifa <2332532718@qq.com>
2019-04-13 11:24:06 +0800

    添加了指令

--- 数据结构/compilation.c
+++ 数据结构/compilation.c
@@ -7,11 +7,9 @@ int token; // current token
 int debug = 1;
 
 // instructions
-enum { 
-    IMM, LC, LI, SC, SI,
-    OR  ,XOR ,AND ,EQ  ,NE  ,LT  ,GT  ,LE  ,GE  ,SHL ,SHR ,ADD ,SUB ,MUL ,DIV ,MOD,
-    PUSH, JMP, JZ, JNZ, EXIT
-};
+enum { LEA ,IMM ,JMP ,CALL,JZ  ,JNZ ,ENT ,ADJ ,LEV ,LI  ,LC  ,SI  ,SC  ,PUSH,
+       OR  ,XOR ,AND ,EQ  ,NE  ,LT  ,GT  ,LE  ,GE  ,SHL ,SHR ,ADD ,SUB ,MUL ,DIV ,MOD ,
+       OPEN,READ,CLOS,PRTF,MALC,MSET,MCMP,EXIT };
 
 /*
 +------------------+
@@ -60,9 +58,19 @@ int eval() {
     while (1) {
         cycle ++;
         op = *pc++;
-
+        // print debug info
+        if (debug) {
+            printf("%d> %.4s", cycle,
+                   & "LEA ,IMM ,JMP ,CALL,JZ  ,JNZ ,ENT ,ADJ ,LEV ,LI  ,LC  ,SI  ,SC  ,PUSH,"
+                   "OR  ,XOR ,AND ,EQ  ,NE  ,LT  ,GT  ,LE  ,GE  ,SHL ,SHR ,ADD ,SUB ,MUL ,DIV ,MOD ,"
+                   "OPEN,READ,CLOS,PRTF,MALC,MSET,MCMP,EXIT"[op * 5]);
+            if (op <= ADJ)
+                printf(" %d\n", *pc);
+            else
+                printf("\n");
+        }
         if (op == IMM) ax = *pc++;
-        else if (op == LC) {ax = *(char *)ax;}
+        else if (op == LC) {ax = *(char *)ax;} // 载入，要求 ax 中是地址
         else if (op == LI) {ax = *(int *)ax;}
         else if (op == SC) {*(char *)sp++ = ax;}
         else if (op == SI) {*(int *)sp++ = ax;}
@@ -71,6 +79,34 @@ int eval() {
         else if (op == JMP) {pc = (int *)pc;} // 将下一条指令载入
         else if (op == JZ) { pc = ax ? pc + 1 : (int *)*pc; }
         else if (op == JNZ) { pc = ax ? (int *)*pc : pc + 1; }
+
+        else if (op == CALL) {*--sp = (int)(pc+1); pc = (int *)*pc;} // call subroutine
+        //else if (op == RET)  {pc = (int *)*sp++;}   // return from subroutine;
+        else if (op == ENT) { *--sp = (int)bp; bp = sp; sp = sp - *pc++;} // 即保存当前的栈指针，同时在栈上保留一定的空间，用以存放局部变
+        else if (op == ADJ) { sp = sp + *pc++; } // add     esp, 12 将调用子函数时压入栈中的数据清除
+        else if (op == LEV) { sp = bp; bp = (int *)*sp++; pc = (int *)*sp++; } // 本质上这个指令并不是必需的，只是我们的指令集中并没有 POP 指令。并且三条指令写来比较麻烦且浪费空间，所以用一个指令代替
+        /*
+sub_function(arg1, arg2, arg3);
+
+|    ....       | high address
++---------------+
+| arg: 1        |    new_bp + 4
++---------------+
+| arg: 2        |    new_bp + 3
++---------------+
+| arg: 3        |    new_bp + 2
++---------------+
+|return address |    new_bp + 1
++---------------+
+| old BP        | <- new BP
++---------------+
+| local var 1   |    new_bp - 1
++---------------+
+| local var 2   |    new_bp - 2
++---------------+
+|    ....       |  low address
+        */
+       else if (op == LEA) {ax = (int)(bp + *pc++);}
 
         else if (op == OR)  {ax = *sp++ | ax;}
         else if (op == XOR) {ax = *sp++ ^ ax;}
@@ -90,6 +126,17 @@ int eval() {
         else if (op == MOD) {ax = *sp++ % ax;}
 
         else if (op == EXIT) { printf("exit(%d)", *sp); return *sp;}
+        else if (op == OPEN) { ax = open((char *)sp[1], sp[0]); }
+        else if (op == CLOS) { ax = close(*sp);}
+        else if (op == READ) { ax = read(sp[2], (char *)sp[1], *sp); }
+        else if (op == PRTF) { tmp = sp + pc[1]; ax = printf((char *)tmp[-1], tmp[-2], tmp[-3], tmp[-4], tmp[-5], tmp[-6]); }
+        else if (op == MALC) { ax = (int)malloc(*sp);}
+        else if (op == MSET) { ax = (int)memset((char *)sp[2], sp[1], *sp);}
+        else if (op == MCMP) { ax = memcmp((char *)sp[2], (char *)sp[1], *sp);}
+        else {
+            printf("unknown instruction:%d\n", op);
+            return -1;
+        }
     }
 }
 
