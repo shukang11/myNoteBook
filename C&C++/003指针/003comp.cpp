ab1786be0a8a50b6869e9bc1871d1e5f4253ea73
jiazifa <2332532718@qq.com>
2019-04-29 15:50:02 +0800

    Xcode Configuration

--- C&C++/003指针/003comp.cpp
+++ C&C++/003指针/003comp.cpp
@@ -9,15 +9,15 @@ int *text, // text segment
     *stack; // stack
 char *datasegment; // sdata segment
 
-int *pc; // 程序计数器
-int *sp; // 指针寄存器
+int *pc; // 程序计数器 存放指令
+int *sp; // 指针寄存器 寄存数据
 int *bp; // 基址指针
 int ax;// 通用寄存器
 
 int cycle;
 
-enum { IMM ,PUSH, 
-    ADD ,SUB ,MUL ,DIV,
+enum { IMM ,PUSH, JMP ,JZ  ,JNZ ,CALL,ENT ,ADJ ,
+    ADD ,SUB ,MUL ,DIV ,
     EXIT
 };
 
@@ -27,7 +27,7 @@ int eval() {
         op =  *pc++;
         cycle ++;
         printf("%d> %.4s ax=%d  *sp = %d\n", cycle,
-                   & "IMM ,PUSH,"
+                   & "IMM ,PUSH,JMP ,JZ  ,JNZ ,CALL,ENT ,ADJ ,"
                    "ADD ,SUB ,MUL ,DIV ,"
                    "EXIT"[op * 5], ax, *sp);
         if (op == IMM) { // 将值移动到通用寄存器MOV(tar sor)
@@ -35,7 +35,23 @@ int eval() {
 
         } else if (op == PUSH) { // sp 的入栈是减小的，将值入栈
             *--sp = ax;
-            
+        // 跳转指令 JMP 
+        } else if (op == JMP) {
+            pc = (int *)*pc; // 将当前指令设置为指定的地址 (也就是下一条指令设置为指定的)
+        } else if (op == JZ) { // （ax）为零或不为零情况下的跳转
+            pc = ax ? pc + 1 : (int *)*pc;
+        } else if (op == JNZ) {
+            pc = ax ? (int *)*pc : pc + 1;
+        } else if (op == CALL) {
+            /*
+             为什么不能直接使用 JMP 指令呢？原因是当我们从子函数中返回时，
+             程序需要回到跳转之前的地方继续运行，这就需要事先将这个位置信息存储起来。
+             反过来，子函数要返回时，就需要获取并恢复这个信息。因此实际中我们将 PC 保存在栈中
+            */
+           *--sp = (int)(pc + 1);
+           pc = (int *)*pc;
+        // } else if (op == RET)  {pc = (int *)*sp++;} 
+
         // 操作数
         } else if (op == ADD) { // +
             ax = *sp++ + ax;
@@ -45,7 +61,7 @@ int eval() {
             ax = *sp++ / ax;
         } else if (op == MUL) { // *
             ax = *sp++ * ax;
-
+        
         // 系统操作
         } else if (op == EXIT) {
             cout << "exit(" << *sp << ")" << endl;
